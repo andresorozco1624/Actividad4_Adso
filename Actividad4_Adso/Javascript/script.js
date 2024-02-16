b2a14109962235b302929de937d82ac0cd782316
andresorozco1624 <108691375+andresorozco1624@users.noreply.github.com>
2024-02-15 22:56:57 -0500

    Feat(Frontend)
    Connecting server to client

---
+++ Actividad4_Adso/Javascript/script.js
@@ -0,0 +1,19 @@
+function getAllUser() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/user");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const data = xhr.response;
+            console.log(data);
+        } else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+}
+
+window.addEventListener("load", (e) => {
+    console.log("Entre");
+    getAllUser();
+});
