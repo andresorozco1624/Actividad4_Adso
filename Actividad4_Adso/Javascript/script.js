30594691cb49c0677ef97e1df31d01afcf4962d2
andresorozco1624 <108691375+andresorozco1624@users.noreply.github.com>
2024-02-20 22:01:58 -0500

    FEX(web) Add Form Submission Protocol

--- Actividad4_Adso/Javascript/script.js
+++ Actividad4_Adso/Javascript/script.js
@@ -1,3 +1,26 @@
+
+(function () {
+    'use strict'
+    console.log("Entre");
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+window.addEventListener("submit", () => { console.log("Entre 2"); })
+
 function getAllUser() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:8080/user");
@@ -9,12 +32,7 @@ function getAllUser() {
             console.log(data);
         } else {
             console.log(`Error: ${xhr.status}`);
-
         }
     };
 }
 
-window.addEventListener("load", (e) => {
-    console.log("Entre");
-    getAllUser();
-});
