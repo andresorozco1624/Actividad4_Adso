eff7cf7266048f64fefb7c6c5272c1e050754bd3
andresorozco1624 <108691375+andresorozco1624@users.noreply.github.com>
2024-02-21 20:14:22 -0500

    Feat(Web): Add conexion between login.html and script.js
    Coonnecting  longin form with form submission protocol

--- Actividad4_Adso/Javascript/script.js
+++ Actividad4_Adso/Javascript/script.js
@@ -1,90 +1,97 @@
+/* This function add bootstrap styles dynamically like is- valid or is-invalid using 
+the method blur()  (which works when we remove focus from an element) 
+to know if the emails are the same or not, 
+this was also applied to the password textboxes */
 
 (function () {
-    document.getElementById("passwordConfirm").addEventListener("blur", (e) => {
-        password = document.getElementById("password");
-        passwordConfirm = document.getElementById("passwordConfirm");
-        passwordFeedback = document.getElementById("passwordFeedback");
-        passwordConfirmFeedback = document.getElementById("passwordConfirmFeedback");
+    try {
+        document.getElementById("passwordConfirm").addEventListener("blur", (e) => {
+            password = document.getElementById("password");
+            passwordConfirm = document.getElementById("passwordConfirm");
+            passwordFeedback = document.getElementById("passwordFeedback");
+            passwordConfirmFeedback = document.getElementById("passwordConfirmFeedback");
 
-        if (password.value != passwordConfirm.value) {
-            console.log("Contraseñas diferentes");
-            password.classList.add("is-invalid");
-            passwordConfirm.classList.add("is-invalid");
+            if (password.value != passwordConfirm.value) {
+                console.log("Contraseñas diferentes");
+                password.classList.add("is-invalid");
+                passwordConfirm.classList.add("is-invalid");
 
-            passwordFeedback.innerHTML = "Las contraseñas no coinciden";
-            passwordFeedback.classList.add("invalid-feedback");
-            passwordFeedback.classList.remove("valid-feedback");
+                passwordFeedback.innerHTML = "Las contraseñas no coinciden";
+                passwordFeedback.classList.add("invalid-feedback");
+                passwordFeedback.classList.remove("valid-feedback");
 
-            passwordConfirmFeedback.classList.add("invalid-feedback");
-            passwordConfirmFeedback.innerHTML = "Las contraseñas no coinciden";
-            passwordConfirmFeedback.classList.remove("valid-feedback");
+                passwordConfirmFeedback.classList.add("invalid-feedback");
+                passwordConfirmFeedback.innerHTML = "Las contraseñas no coinciden";
+                passwordConfirmFeedback.classList.remove("valid-feedback");
+
+            }
+            else {
+                console.log("Contraseñas iguales");
+                password.classList.remove("is-invalid");
+                passwordConfirm.classList.remove("is-invalid");
+                password.classList.add("is-valid");
+                passwordConfirm.classList.add("is-valid");
+
+
+
+                passwordFeedback.innerHTML = "Las contraseñas iguales";
+                passwordFeedback.classList.remove("invalid-feedback");
+                passwordFeedback.classList.add("valid-feedback");
+
+
+
+                passwordConfirmFeedback.innerHTML = "Las contraseñas iguales";
+                passwordConfirmFeedback.classList.remove("invalid-feedback");
+                passwordConfirmFeedback.classList.add("valid-feedback");
+            }
 
         }
-        else {
-            console.log("Contraseñas iguales");
-            password.classList.remove("is-invalid");
-            passwordConfirm.classList.remove("is-invalid");
-            password.classList.add("is-valid");
-            passwordConfirm.classList.add("is-valid");
+        );
+        document.getElementById("emailConfirm").addEventListener("blur", (e) => {
+            email = document.getElementById("email");
+            emailConfirm = document.getElementById("emailConfirm");
+
+            if (email.value != emailConfirm.value) {
+                console.log("Correos diferentes");
+                email.classList.add("is-invalid");
+                emailConfirm.classList.add("is-invalid");
+
+                emailFeedback.innerHTML = "Los correos no coinciden";
+                emailFeedback.classList.add("invalid-feedback");
+                emailFeedback.classList.remove("valid-feedback");
+
+                emailConfirmFeedback.classList.add("invalid-feedback");
+                emailConfirmFeedback.innerHTML = "Los correos no coinciden";
+                emailConfirmFeedback.classList.remove("valid-feedback");
+            }
+            else {
+                console.log("Correos iguales");
+
+                email.classList.remove("is-invalid");
+                emailConfirm.classList.remove("is-invalid");
+                email.classList.add("is-valid");
+                emailConfirm.classList.add("is-valid");
 
 
 
-            passwordFeedback.innerHTML = "Las contraseñas iguales";
-            passwordFeedback.classList.remove("invalid-feedback");
-            passwordFeedback.classList.add("valid-feedback");
+                emailFeedback.innerHTML = "Los correos son iguales";
+                emailFeedback.classList.remove("invalid-feedback");
+                emailFeedback.classList.add("valid-feedback");
 
 
 
-            passwordConfirmFeedback.innerHTML = "Las contraseñas iguales";
-            passwordConfirmFeedback.classList.remove("invalid-feedback");
-            passwordConfirmFeedback.classList.add("valid-feedback");
+                emailConfirmFeedback.innerHTML = "Los correos son iguales";
+                emailConfirmFeedback.classList.remove("invalid-feedback");
+                emailConfirmFeedback.classList.add("valid-feedback");
+            }
         }
+        );
+    } catch (error) {
 
     }
-    );
-
-    document.getElementById("emailConfirm").addEventListener("blur", (e) => {
-        email = document.getElementById("email");
-        emailConfirm = document.getElementById("emailConfirm");
-
-        if (email.value != emailConfirm.value) {
-            console.log("Correos diferentes");
-            email.classList.add("is-invalid");
-            emailConfirm.classList.add("is-invalid");
-
-            emailFeedback.innerHTML = "Los correos no coinciden";
-            emailFeedback.classList.add("invalid-feedback");
-            emailFeedback.classList.remove("valid-feedback");
-
-            emailConfirmFeedback.classList.add("invalid-feedback");
-            emailConfirmFeedback.innerHTML = "Los correos no coinciden";
-            emailConfirmFeedback.classList.remove("valid-feedback");
-        }
-        else {
-            console.log("Correos iguales");
-
-            email.classList.remove("is-invalid");
-            emailConfirm.classList.remove("is-invalid");
-            email.classList.add("is-valid");
-            emailConfirm.classList.add("is-valid");
-
-
-
-            emailFeedback.innerHTML = "Los correos son iguales";
-            emailFeedback.classList.remove("invalid-feedback");
-            emailFeedback.classList.add("valid-feedback");
-
-
-
-            emailConfirmFeedback.innerHTML = "Los correos son iguales";
-            emailConfirmFeedback.classList.remove("invalid-feedback");
-            emailConfirmFeedback.classList.add("valid-feedback");
-        }
-    }
-    );
 }());
 
-
+/* Form Submission Protocol: This doesn't allow to submit the form, if it is empty */
 (function () {
     'use strict'
 
